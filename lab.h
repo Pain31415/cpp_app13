880bf90ee19999a7e3e406331a4b3b71199fda08
Pain31415 <Cherniy412@ukr.net>
2023-11-14 18:56:39 +0200

    Add project files.

---
+++ lab.h
@@ -0,0 +1,21 @@
+﻿#ifndef LIB_H
+#define LIB_H
+
+constexpr auto zhongwen = "中文";
+
+#include <cmath>
+#include <cstdlib>
+
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+#define SQUARE(x) ((x) * (x))
+
+#define POWER(x, n) pow((x), (n))
+
+#define IS_EVEN(x) ((x) % 2 == 0)
+
+#define IS_ODD(x) ((x) % 2 != 0)
+
+#endif // LIB_H
---
+++ main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "lab.h"
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+
+    SetConsoleOutputCP(CP_UTF8);
+
+    srand(time(nullptr));
+
+    int a = rand() % 100;
+    int b = rand() % 100;
+
+    cout << u8"Менше з " << a << u8" і " << b << u8": " << MIN(a, b) << endl;
+    cout << u8"Більше з " << a << u8" і " << b << u8": " << MAX(a, b) << endl;
+    cout << a << u8" 在一个正方形里: " << SQUARE(a) << endl;
+    cout << a << u8" 提高到 3 次方: " << POWER(a, 3) << endl;
+
+    if (IS_EVEN(a))
+        cout << a << u8" - 偶数." << endl;
+    else
+        cout << a << u8" - 奇数." << endl;
+
+    if (IS_ODD(b))
+        cout << b << u8" - 奇数." << endl;
+    else
+        cout << b << u8" - 偶数." << endl;
+
+    return 0;
+}
